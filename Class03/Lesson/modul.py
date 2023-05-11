04bbf7ba73c701f6144b0841cfcff05596fddd57
DenisM <mdr2010@yandex.ru>
2023-05-12 00:38:22 +0500

    add Lesson03

--- Class03/Lesson/modul.py
+++ Class03/Lesson/modul.py
@@ -11,3 +11,11 @@ def max1(a, b):
     if a > b:
         return a # return прерывает программу поэтому else ставить не надо
     return b
+
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    return
